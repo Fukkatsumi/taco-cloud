3f6dea1bcb869acd71909a97f494a951e822ce11
Fukkatsumi <fukkatsumi@mail.ua>
2020-11-10 02:09:24 +0200

    section-2-2 Processing form submission

--- src/main/java/com/fukkatsumi/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/fukkatsumi/tacocloud/controller/DesignTacoController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -38,6 +39,14 @@ public class DesignTacoController {
         }
         model.addAttribute("design", new Taco());
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
