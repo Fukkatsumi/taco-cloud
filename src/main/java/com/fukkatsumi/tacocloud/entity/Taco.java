56ca1ab802db7791bd1f75cc2195c793c89bae14
Fukkatsumi <fukkatsumi@mail.ua>
2020-11-19 23:10:40 +0200

    section-3-1 Reading and writing data with JDBC

--- src/main/java/com/fukkatsumi/tacocloud/entity/Taco.java
+++ src/main/java/com/fukkatsumi/tacocloud/entity/Taco.java
@@ -1,17 +1,25 @@
 package com.fukkatsumi.tacocloud.entity;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
+@RequiredArgsConstructor
 public class Taco {
+
+    private Long id;
+
+    private Date createdAt;
+
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
