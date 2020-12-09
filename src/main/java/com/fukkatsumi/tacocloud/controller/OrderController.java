4fa125d0cd0e42ee3f2470fca39586c1226a60be
Fukkatsumi <fukkatsumi@mail.ua>
2020-12-09 19:36:32 +0200

    section-5-2 Creating your own configuration properties

--- src/main/java/com/fukkatsumi/tacocloud/controller/OrderController.java
+++ src/main/java/com/fukkatsumi/tacocloud/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.fukkatsumi.tacocloud.controller;
 import com.fukkatsumi.tacocloud.entity.Order;
 import com.fukkatsumi.tacocloud.entity.User;
 import com.fukkatsumi.tacocloud.repository.OrderRepository;
+import com.fukkatsumi.tacocloud.utils.OrderProps;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -17,9 +21,11 @@ import javax.validation.Valid;
 public class OrderController {
 
     private OrderRepository orderRepo;
+    private OrderProps props;
 
-    public OrderController(OrderRepository orderRepo) {
+    public OrderController(OrderRepository orderRepo, OrderProps props) {
         this.orderRepo = orderRepo;
+        this.props = props;
     }
 
     @GetMapping("/current")
@@ -42,6 +48,15 @@ public class OrderController {
         }
 
         return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders",
+                orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @PostMapping
