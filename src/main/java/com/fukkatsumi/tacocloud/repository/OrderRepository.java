1af0eeb16e70d513d780d29350d8fcf17d3c6487
Bogachenko Nastya <30742460+Fukkatsumi@users.noreply.github.com>
2020-12-11 18:12:38 +0200

    section-5-2 Creating your own configuration properties (#7)

--- src/main/java/com/fukkatsumi/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/fukkatsumi/tacocloud/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.fukkatsumi.tacocloud.repository;
 
 import com.fukkatsumi.tacocloud.entity.Order;
+import com.fukkatsumi.tacocloud.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 //    List<Order> findByDeliveryZip(String deliveryZip);
 //
